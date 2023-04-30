d339aa071e8405012bf1f9b2751fc6bae39e1aaa
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2023-04-30 14:28:44 -0700

    changes for v4

--- src/pages/read-paper.js
+++ src/pages/read-paper.js
@@ -32,7 +32,7 @@ export default function ReadPaper() {
      <AppBar position="fixed" color="secondary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          LGB 🧬 use on a laptop or desktop monitor
+          LGB 🧬
         </Typography>
       </Toolbar>
     </AppBar>
--- src/pages/select-paper.js
+++ src/pages/select-paper.js
@@ -71,7 +71,7 @@ export default function SelectPaper() {
      <AppBar position="fixed" color="secondary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          LGB 🧬 use on a laptop or desktop monitor
+          LGB 🧬
         </Typography>
       </Toolbar>
     </AppBar>
