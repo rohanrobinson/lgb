69f608351ee1515f3c2cd9b9c88fd17d98b08df8
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2023-05-04 17:17:53 -0700

    resized instruction, paper text on past papers pg

--- src/pages/past-papers.js
+++ src/pages/past-papers.js
@@ -35,23 +35,19 @@ export default function PastPapers() {
         </Toolbar>
       </AppBar>
 
-      <Typography variant="h2" sx={{ mb: 4 }}>
-        🧬 <b>Let's Go Biotech</b> 🚀
-      </Typography>
-
       <Typography variant="body1" sx={{ mb: 2, fontSize: "2.0rem"  }}>
-          Below are past papers that have been featured, check em' out.
-          The paper you select will open up in a new tab.      
+          <span className={styles.lgbDescription}>Below are past papers that have been featured, check em' out.
+          The paper you select will open up in a new tab.</span>     
       </Typography> 
     
     
     <div>
-        <a href="https://pubmed.ncbi.nlm.nih.gov/16904174/" target="_blank"><p>Yamanka Factors</p></a>
-        <a href="https://www.nature.com/articles/s41525-020-0115-6#:~:text=FHIR%20Genomics%20has%20been%20shown,into%20powerful%20precision%20medicine%20apps." target="_blank"><p>FHIR Genomics Standard</p></a>
-        <a href="https://www.nature.com/articles/s41586-023-05881-4" target="_blank"><p>Foundation Generalist Medical AI Models</p></a>
-        <a href="https://www.nature.com/articles/s41587-023-01744-5" target="_blank"><p>CRISPR edited Lung Cells</p></a>
-        <a href="https://www.nature.com/articles/s41587-023-01788-7" target="_blank"><p>Customizing LLMs to Discover Drugs </p></a>
-        <a href="https://www.nature.com/articles/s41587-022-01652-0" target="_blank"><p>Simultaneous DNA & Epigenetic Sequencing</p></a>
+        <a href="https://pubmed.ncbi.nlm.nih.gov/16904174/" target="_blank"><p><span className={styles.lgbText}>Yamanka Factors</span></p></a>
+        <a href="https://www.nature.com/articles/s41525-020-0115-6#:~:text=FHIR%20Genomics%20has%20been%20shown,into%20powerful%20precision%20medicine%20apps." target="_blank"><p><span className={styles.lgbText}>FHIR Genomics Standard</span></p></a>
+        <a href="https://www.nature.com/articles/s41586-023-05881-4" target="_blank"><p><span className={styles.lgbText}>Foundation Generalist Medical AI Models</span></p></a>
+        <a href="https://www.nature.com/articles/s41587-023-01744-5" target="_blank"><p><span className={styles.lgbText}>CRISPR edited Lung Cells</span></p></a>
+        <a href="https://www.nature.com/articles/s41587-023-01788-7" target="_blank"><p><span className={styles.lgbText}>Customizing LLMs to Discover Drugs</span></p></a>
+        <a href="https://www.nature.com/articles/s41587-022-01652-0" target="_blank"><p><span className={styles.lgbText}>Simultaneous DNA & Epigenetic Sequencing</span></p></a>
     </div>
 
     <br />
