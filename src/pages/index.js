e7024fcf61f2fa9c2d45c65c9b88c52a205aa29b
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2025-12-06 16:23:22 -0600

    link to trial hero site

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   const topicList = ["Genetics", "Fertility", "Cancer", "Aging", "Artificial Intelligence", "Diagnostics", "Medical Devices", "Bioinformatics", "Immunology"];
 
-   
+
   const handleTopicClick = (topicName) => {
     setSelectedTopic(topicName);
   };
@@ -27,33 +27,31 @@ export default function Home() {
   // };
 
   return (
-      <div>
-        <Navbar />
-        <h1>Select a topic you're interested in learning about!</h1>
-        <div className="topicsGrid">
-          {topicList.map(topic => (
-            <Topic key={topic} topicName={topic} 
+    <div>
+      <Navbar />
+      <h1>Select a topic you're interested in learning about!</h1>
+      <div className="topicsGrid">
+        {topicList.map(topic => (
+          <Topic key={topic} topicName={topic}
 
-                onClick={() => {
-                  router.push({
-                    pathname: '/Learn',
-                    query: { topic: topic }, // Pass the selected topic as a query parameter
-                  });
-                }} 
-            // onClick= {() => handleTopicClick(topic)} 
-            />
-          ))}
+            onClick={() => {
+              router.push({
+                pathname: '/Learn',
+                query: { topic: topic }, // Pass the selected topic as a query parameter
+              });
+            }}
+          // onClick= {() => handleTopicClick(topic)} 
+          />
+        ))}
 
-          <br />
-          <br />
+        <br />
+        <br />
 
-          <button className="topicTile" onClick={() => {
-            router.push({
-              pathname: '/TrialForge',
-            });
-          }}><b>🧪 Curious about Interesting Clinical Trials?</b></button>
-        </div>
-        {/* {selectedTopic && (
+        <button className="topicTile" onClick={() => {
+          window.open('https://trialhero.vercel.app', '_blank');
+        }}><b>🧪 Curious about Interesting Clinical Trials?</b></button>
+      </div>
+      {/* {selectedTopic && (
           <div className="modalOverlay" onClick={closeModal}>
             <div className="modalContent" onClick={e => e.stopPropagation()}>
               <h2>{selectedTopic}</h2>
